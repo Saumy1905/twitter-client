c0df8756f4bbeb93f2df05f1874c691fcb2a44cf
Saumy1905 <saumy1905@gmail.com>
2024-11-14 15:55:40 +0530

    SideBar options added using Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import React from "react";
 import Image from "next/image";
 import { BsTwitterX } from "react-icons/bs";
+import { GoHome } from "react-icons/go";
+import { LuSearch } from "react-icons/lu";
+import { HiOutlineBell } from "react-icons/hi2";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { FaRegBookmark } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] })
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,13 +23,63 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+interface TwitterSidebarButton {
+  title: string
+  icon: React.ReactNode
+}
+
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: 'Home',
+    icon: <GoHome />
+  },
+  {
+    title: 'Explore',
+    icon: <LuSearch />
+  },
+  {
+    title: 'Notifications',
+    icon: <HiOutlineBell />
+  },
+  {
+    title: 'Messages',
+    icon: <FaRegEnvelope />
+  },
+  {
+    title: 'Bookmarks',
+    icon: <FaRegBookmark />
+  },
+  {
+    title: 'Profile',
+    icon: <CiUser />
+  }
+]
+
 export default function Home() {
   return (
-    <div>
+    <div className="inter.className">
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3 flex justify-start pt-8">
-          <div className="text-4xl h-fit hover:bg-gray-600 rounded-full p-2 cursor-pointer transition-all">
-            <BsTwitterX/>
+        <div className="col-span-3 pt-8 px-4">
+          <div className="text-4xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+            <BsTwitterX />
+          </div>
+          <div className="mt-4 text-xl pr-4">
+            <ul>
+              {sidebarMenuItems.map(item => (
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-2 w-fit cursor-pointer mt-2"
+                  key={item.title}
+                >
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-5">
+              <button className="bg-[#1d9bf0] text-lg font-semibold p-2 rounded-full w-full ">
+                Post
+              </button>
+            </div>
           </div>
         </div>
         <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-400"></div>
